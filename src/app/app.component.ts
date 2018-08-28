057c5a2fea686d8907b555261ba53c159ec305ba
vaklarma <biro.andrasgabor.regist@gmail.com>
2018-08-28 23:34:07 +0200

    Nah ez már felmegy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'jegybazar-reload ez egy újrakezdés';
+  title = 'jegybazar-reload ez egy újrakezdés megint';
 }
