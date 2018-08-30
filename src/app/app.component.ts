ccf087d787e19b9cedd3f4b22221f4c7a800d179
vaklarma <biro.andrasgabor.regist@gmail.com>
2018-08-30 20:08:41 +0200

    - Tömb létrehozása inputból  - reduce függvény,  - map  - filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {split} from 'ts-node';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'jegybazar-reload ez egy újrakezdés megint';
+  convertedArray = [];
+  sumResult;
+
+
+  getSum(value) {
+
+    //  const fg = (accu, curr) => accu + curr;
+    //  this.disp = this.tomb.reduce((accu, curr) => accu + curr);
+
+    this.convertedArray = this.convertInput(value);
+    this.sumResult = this.convertedArray.reduce(function (accu, curr) {
+      return (accu + curr);
+    });
+
+  }
+
+  convertInput(value: string) {
+    return value.split(',')
+      .map(Number)
+      .filter(x => !isNaN(x));
+  }
+  onKey(value: string) {
+this.getSum(value);
+  }
 }
